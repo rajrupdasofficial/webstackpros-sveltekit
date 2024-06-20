3b9f59005aac97c41edb6cad8c8f0b375e418b39
RajrupDasid <rajrupofficialid@protonmail.com>
2024-06-20 20:04:03 +0530

    Group based layout added
    default layout changed
    
    some auth logic improvement added
    
    admin navbar added
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ src/routes/(admin)/admin/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import Adminnavbar from "@/components/ui/admin/navbar/+adminnavbar.svelte";
+import { ModeWatcher } from "mode-watcher";
+import "./admin.css"
+</script>
+<ModeWatcher/>
+<Adminnavbar/>
+
+<slot></slot>
---
+++ src/routes/(admin)/admin/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { onMount } from 'svelte';
+let user;
+onMount(async()=>{
+    const response = await fetch('/api/auth/verifyauth',{
+        credentials:'include'
+    });
+    const user = response.headers.get('user-status');
+    const parsedata = JSON.parse(user)
+    const isadmin = parsedata.isadmin;
+    console.log(isadmin)
+    if(isadmin===false){
+        window.location.href="/"
+    }
+})
+</script>
+
+<svelte:head>
+    <title>webstackpros | Admin Dashboard</title>
+</svelte:head>
+<h1>Welcome to admin Dashboard</h1>
+
