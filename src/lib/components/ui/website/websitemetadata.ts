ddb389ac8736a845886bea3f339d8af03d4117e2
RajrupDasid <rajrupofficialid@protonmail.com>
2024-06-21 20:58:11 +0530

    SEO added
    some general improvements added
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ src/lib/components/ui/website/websitemetadata.ts
@@ -0,0 +1,18 @@
+export const websiteMetadata = {
+	title: 'Websatckpros - innovative Tech Solutions for a connected world',
+	author: 'Webstackpros',
+	headerTitle: 'Webstackpros network',
+	description:
+		'Welcome to our digital canvas, where innovation and creativity converge. With a keen eye for aesthetics and a mastery of code, our portfolio showcases a diverse collection of projects that reflect our commitment to excellence',
+	language: 'en-us',
+	theme: 'system', // system, dark or light
+	siteUrl: 'https://www.webstackpros.net', // your website URL
+	siteLogo: '/favicon.ico',
+	socialBanner: '/heros2.png', // add social banner in the public folder
+	email: 'info@webstackpros.net',
+	facebook: 'https://www.facebook.com/profile.php?id=61558804966278',
+	instgram: 'https://www.instagram.com/webstackprosofficial/',
+	youtube: 'https://youtube.com/webstackprosnetwork',
+	linkedin: 'https://in.linkedin.com/company/webstackpros-net',
+	locale: 'en-US'
+};
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,25 +5,89 @@ import Webstitecarousel from "@/components/ui/website/+webstitecarousel.svelte";
 import Appoffer from "@/components/ui/appoffer/+appoffer.svelte"
 import Ourservices from "@/components/ui/ourservices/+ourservices.svelte";	
 import Portfolio from "@/components/ui/portfolio/+portfolio.svelte";
-// import { onMount } from 'svelte';
-// let user;
-// onMount(async () => {
-//         const response = await fetch('/api/auth/verifyauth', {
-//             credentials: 'include'
-//         });
-//         // const user = response.headers.get('user-status');
-        
-//         // const parsedata = JSON.parse(user)
-//         // const userid = parsedata.userid
-//         // console.log(userid)
+import { websiteMetadata } from "@/components/ui/website/websitemetadata";
 
-//         if (response.status===401){
-//             window.location.href="/auth/login";
-//         }
 
-        
-//     });
+export const metadata = {
+  metadataBase: new URL("https://www.webstackpros.net"),
+  title: websiteMetadata.title,
+  description:
+    websiteMetadata.description,
+  other: { "p:domain_verify": "7f562b83bb6b61abb8204072b470b756" },
+  openGraph: {
+    title: websiteMetadata.title,
+    description: websiteMetadata.description,
+    url: websiteMetadata.siteUrl,
+    siteName: websiteMetadata.title,
+    images: [websiteMetadata.socialBanner],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: websiteMetadata.title,
+    images: [websiteMetadata.socialBanner],
+  },
+
+};
+
+
+
 </script>
+
+<svelte:head>
+<title>{metadata.title}</title>
+<meta name="description" content={metadata.description}>
+<meta name="keywords" content="webstackpros, best web development company,best web development company in kolkata, Domain purchase,web hosting,Web development , andorid development, best IT company for web development, best IT company from android development, best web hosting ">
+<meta property="og:title" content={metadata.title} />
+<meta property="og:description" content={metadata.description} />
+<meta property="og:image" content={websiteMetadata.socialBanner} />
+<meta name="robots" content="index,follow" />
+<meta name="googlebot" content="index, follow" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content={metadata.title} />
+<meta name="twitter:description" content={metadata.description} />
+<meta name="twitter:image" content={websiteMetadata.socialBanner} />
+<script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-B9MHZ1HXV0">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+    gtag('config', 'G-B9MHZ1HXV0')
+  </script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4377761625773230"
+  crossorigin="anonymous"></script>
+  <script>
+    (function(c,l,a,r,i,t,y){
+                c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+                t=l.createElement(r);
+                t.async=1;
+                t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "j9zlm6r4eh");
+  </script>
+</svelte:head>
+
 
 <!-- header carousel section -->
 <div class="lg:ml-[70px] lg:mt-[60px] sm:ml[10px]">
