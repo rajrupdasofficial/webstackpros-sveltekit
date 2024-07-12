37efad383404b8c2e9d2ae0aca23207b35f8f224
RajrupDasid <rajrupofficialid@protonmail.com>
2024-07-12 08:51:23 +0530

    fixed a small typo
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/lib/firebase/firebasefileupload.ts
+++ src/lib/firebase/firebasefileupload.ts
@@ -1,9 +1,9 @@
 import { getStorage, ref, uploadBytes } from 'firebase/storage';
-import { firebase_cofig } from './firebaseconfig';
+import { firebase_config } from './firebaseconfig';
 
 export const uploadImageToFirebase = async (imageData, uniqueFilename) => {
 	try {
-		const storage = getStorage(firebase_cofig);
+		const storage = getStorage(firebase_config);
 		const storageRef = ref(storage, `${uniqueFilename}`);
 		console.log('storageref', storageRef);
 		await uploadBytes(storageRef, imageData);
